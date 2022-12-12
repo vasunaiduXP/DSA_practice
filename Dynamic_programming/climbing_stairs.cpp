7cd0e43c643c4f172daa336dc90b03565b14843a
vasunaiduXP <58740730+vasunaiduXP@users.noreply.github.com>
2022-12-12 15:29:51 +0530

    Update climbing_stairs.cpp

--- Dynamic_programming/climbing_stairs.cpp
+++ Dynamic_programming/climbing_stairs.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+#
+int dp[100000009]={0};
 
+int countDistinctWays(int n) {
+    dp[0]=dp[1]=1;
+    
+    //  Write your code here.
+    if(n==0) return 1;
+    if(n==1) return 1;
+    if(dp[n]) return dp[n];
+    int t=(countDistinctWays(n-1) + countDistinctWays(n-2));
+    return dp[n]=t%(int)(pow(10,9)+7);
+}
+
+
+/*
+
+https://www.codingninjas.com/codestudio/problems/count-ways-to-reach-nth-stairs_798650
+
+*/
