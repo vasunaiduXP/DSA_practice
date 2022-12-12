d9be585e25a2e092c229cafdf4a5be9b568cee38
vasunaiduXP <58740730+vasunaiduXP@users.noreply.github.com>
2022-12-12 16:35:30 +0530

    Update forg_jump.cpp

--- Dynamic_programming/forg_jump.cpp
+++ Dynamic_programming/forg_jump.cpp
@@ -18,6 +18,25 @@ int fun(int n, vector<int> &ht, vector<int> &dp) {
     return dp[n] = min(left, right);
 }
 
+//-----------------------------------------------------------------------------------------------------------------------------
+
+#include <bits/stdc++.h> 
+int fun(int n , vector<int> &ht, vector<int> &dp);
+
+int frogJump(int n, vector<int> &heights)
+{    vector<int> dp(n, 0);
+     dp[0] = 0;
+     for(int i=1;i<n;++i) {
+         int left = dp[i-1] + abs(heights[i]-heights[i-1]);
+         int right = INT_MAX;
+         if(i>1) right = dp[i-2]+abs(heights[i]-heights[i-2]);
+         
+         dp[i]=min(left, right);
+     }
+    return dp[n-1];
+}
+
+
 /*
 
 Sample Input 1:
