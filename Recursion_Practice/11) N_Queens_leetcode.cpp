0a65b3e1b78d317c55ba84413a3f652f38a76d2f
vasunaiduXP <58740730+vasunaiduXP@users.noreply.github.com>
2022-12-17 21:01:12 +0530

    Create 11) N_Queens_leetcode.cpp

---
+++ Recursion_Practice/11) N_Queens_leetcode.cpp
@@ -0,0 +1,137 @@
+//https://leetcode.com/problems/n-queens/
+
+//Method-1
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> board(n);
+        string s(n, '.');
+ 
+        for(int i=0;i<n;++i) board[i]=s;
+ 
+        solve(0, board, res, n);
+        return res;
+    }
+ 
+    void solve(int col, vector<string> &board, vector<vector<string>> &res, int n ) {
+        if(col>=n) {
+            res.push_back(board);
+            return;
+        }
+ 
+        for(int row=0;row<n;++row) {
+            if(isSafe(row, col, board, n)) {
+                board[row][col]='Q';
+                solve(col+1, board, res, n);
+                board[row][col]='.';
+            }
+        }
+    }
+ 
+    bool isSafe(int row, int col, vector<string> board, int n) {
+        int duprow=row;
+        int dupcol=col;
+ 
+        while(col>=0 && row>=0) {
+            if(board[row][col]=='Q')
+                return false;
+            --row;
+            --col;
+        }
+ 
+        row = duprow;
+        col = dupcol;
+ 
+        while(col>=0) {
+            if(board[row][col]=='Q')
+                return false;
+            --col;
+        }
+       
+        row = duprow;
+        col = dupcol;
+       
+        while(row<n && col>=0) {
+            if(board[row][col]=='Q')
+                return false;
+            ++row;
+            --col;
+        }
+ 
+        return true;
+    }
+ 
+};
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------
+
+//Method-2
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> board(n);
+        string s(n, '.');
+ 
+        for(int i=0;i<n;++i) board[i]=s;
+ 
+        vector<int> leftRow(n,0), upperDiagonal(2*n-1, 0), lowerDiagonal(2*n-1, 0);
+        solve(0, board, res, n, leftRow, upperDiagonal, lowerDiagonal);
+        return res;
+    }
+ 
+    void solve(int col, vector<string> &board, vector<vector<string>> &res, int n , vector<int> &leftRow, vector<int> &upperDiagonal, vector<int> &lowerDiagonal) {
+        if(col>=n) {
+            res.push_back(board);
+            return;
+        }
+ 
+        for(int row=0;row<n;++row) {
+            if(leftRow[row]==0 && upperDiagonal[row+col]==0 && lowerDiagonal[n-1+col-row]==0) {
+                board[row][col]='Q';
+                leftRow[row]=1;
+                upperDiagonal[row+col]=1;
+                lowerDiagonal[n-1 + col-row]=1;
+                solve(col+1, board, res, n, leftRow, upperDiagonal, lowerDiagonal);
+                board[row][col]='.';
+                leftRow[row]=0;
+                upperDiagonal[row+col]=0;
+                lowerDiagonal[n-1 + col-row]=0;
+            }
+        }
+    }
+ 
+ 
+};
+
+
+/*
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+ 
+
+Example 1:
+
+
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+Example 2:
+
+Input: n = 1
+Output: [["Q"]]
+ 
+
+Constraints:
+
+1 <= n <= 9
+
+*/
